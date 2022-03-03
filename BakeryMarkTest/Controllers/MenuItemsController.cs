fcdfd2d87c35ef00c8791515208183f399a88322
Mark Pavlyuk <mmarikk.violinist1@gmail.com>
2022-03-03 00:56:04 -0800

    added search to the menu

--- BakeryMarkTest/Controllers/MenuItemsController.cs
+++ BakeryMarkTest/Controllers/MenuItemsController.cs
@@ -1,13 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
-using Microsoft.EntityFrameworkCore;
-using BakeryMarkTest.Data;
+﻿using BakeryMarkTest.Data;
 using BakeryMarkTest.Models;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace BakeryMarkTest.Controllers
 {
@@ -24,6 +21,18 @@ namespace BakeryMarkTest.Controllers
         public async Task<IActionResult> Index()
         {
             return View(await _context.MenuItem.ToListAsync());
+        }
+
+        // GET: SearchForMenuItem
+        public async Task<IActionResult> SearchForMenuItem()
+        {
+            return View();
+        }
+
+        // POST: ShowSearchResult
+        public async Task<IActionResult> ShowSearchResult(string SearchItem)
+        {
+            return View("Index", await _context.MenuItem.Where( j => j.Name.Contains(SearchItem)).ToListAsync());
         }
 
         // GET: MenuItems/Details/5
@@ -45,7 +54,7 @@ namespace BakeryMarkTest.Controllers
         }
 
         // GET: MenuItems/Create
-        //[Authorize(Roles = "Administrator")]
+        [Authorize(Roles = "Administrator")]
         public IActionResult Create()
         {
             return View();

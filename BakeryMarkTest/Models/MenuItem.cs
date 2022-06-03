ff9eaa76055c474c1bde379421a7eb18edb273f3
Matthew <konenmatt@gmail.com>
2022-06-03 00:03:02 -0700

    Added some extra model validation.

--- BakeryMarkTest/Models/MenuItem.cs
+++ BakeryMarkTest/Models/MenuItem.cs
@@ -4,6 +4,8 @@ namespace BakeryMarkTest.Models
 {
     public class MenuItem
     {
+
+        [RegularExpression(@"^[0-9]{1,5}$")]
         public string MenuItemId { get; set; }
         [Required]
         [StringLength(60, ErrorMessage = "Name should be no more than 60 characters.")]
@@ -14,11 +16,15 @@ namespace BakeryMarkTest.Models
         [Range(1, double.MaxValue, ErrorMessage = "Must be greater than 1 or 1")]
         public string Price { get; set; }
         [Required]
+        [RegularExpression(@"^[0-9]{1,5}$")]
         public string NumberOfCalories { get; set; }
         [Required]
+        [RegularExpression(@"^(Not Vegan|Vegan)$")]
         public string IsVegan { get; set; }
         [Required]
+        [RegularExpression(@"^(Not Vegetarian|Vegetarian)$")]
         public string IsVegeterian { get; set; }
+        
         public string ImagePath { get; set; }
 
         public MenuItem()
